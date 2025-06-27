436a75512f28d5bade7a1b40f5b35d38bc327450
AlexMillanG <132949276+AlexMillanG@users.noreply.github.com>
2025-06-26 22:27:49 -0600

    [GIT INIT] subida inicial del proyecto

---
+++ src/main/java/mx/edu/utez/rbbackcomite/RbBackComiteApplication.java
@@ -0,0 +1,13 @@
+package mx.edu.utez.rbbackcomite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RbBackComiteApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RbBackComiteApplication.class, args);
+    }
+
+}
---
+++ src/test/java/mx/edu/utez/rbbackcomite/RbBackComiteApplicationTests.java
@@ -0,0 +1,13 @@
+package mx.edu.utez.rbbackcomite;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RbBackComiteApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
